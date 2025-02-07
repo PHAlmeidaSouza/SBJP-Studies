c6910a62e007695a475519141f34f831b4028252
Pedrou Souza <pedro.asouzavmix@gmail.com>
2025-02-07 14:23:29 -0300

    Project created
    Signed-off-by: Pedrou Souza <pedro.asouzavmix@gmail.com>

---
+++ backend/src/main/java/com/erudio/SBJP_Studies/SbjpStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.erudio.SBJP_Studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbjpStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbjpStudiesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/erudio/SBJP_Studies/SbjpStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erudio.SBJP_Studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbjpStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
