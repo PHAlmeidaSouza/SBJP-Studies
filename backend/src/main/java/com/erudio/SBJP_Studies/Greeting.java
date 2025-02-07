aa84c0f86d94a42dd0516884f12c17ac77921000
Pedrou Souza <pedro.asouzavmix@gmail.com>
2025-02-07 15:41:11 -0300

    First endpoint created
    Signed-off-by: Pedrou Souza <pedro.asouzavmix@gmail.com>

---
+++ backend/src/main/java/com/erudio/SBJP_Studies/Greeting.java
@@ -0,0 +1,13 @@
+package com.erudio.SBJP_Studies;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+    private final long id;
+    private final String content;
+}
