2ca6bfcd9c410fdf429a85f28ecf5f5dbb4710cf
Pedrou Souza <pedro.asouzavmix@gmail.com>
2025-02-10 10:06:54 -0300

    ExceptionHandler implemented
    Signed-off-by: Pedrou Souza <pedro.asouzavmix@gmail.com>

--- backend/src/main/java/com/erudio/SBJP_Studies/MathController.java
+++ backend/src/main/java/com/erudio/SBJP_Studies/MathController.java
@@ -1,5 +1,6 @@
 package com.erudio.SBJP_Studies;
 
+import com.erudio.SBJP_Studies.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +20,7 @@ public class MathController {
             @PathVariable(value = "numberTwo") String numberTwo
     ) throws Exception {
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
