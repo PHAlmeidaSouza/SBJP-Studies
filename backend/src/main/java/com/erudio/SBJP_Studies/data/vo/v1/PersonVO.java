644c00a76834e31112db55544505f3588eef1b50
Pedro Henrique <ph8584774@gmail.com>
2025-02-16 00:24:38 -0300

    Custom JSON Serialization and pom errors fixing

--- backend/src/main/java/com/erudio/SBJP_Studies/data/vo/v1/PersonVO.java
+++ backend/src/main/java/com/erudio/SBJP_Studies/data/vo/v1/PersonVO.java
@@ -1,20 +1,32 @@
 package com.erudio.SBJP_Studies.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"})
 public class PersonVO implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
+
     private String address;
+
     private String gender;
 
 }
